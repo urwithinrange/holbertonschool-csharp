f71f8a8cdb65af65d52201481b34f1ee1645003b
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-12 12:30:10 -0500

    setup of new repo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace holbertonschool_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
