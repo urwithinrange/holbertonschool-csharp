bfc2fd8a51a531d8d0143be2ee27797ee534c074
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-12 13:43:43 -0500

    doing it the long way

--- 0x00-csharp-hello_world/0-initialize_new_project.sh
+++ 0x00-csharp-hello_world/0-initialize_new_project.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 # script that inits a new C# proj inside a fldr titled 0-new_project
-dotnet new console -o 0-new_project
+
+mkdir 0-new_project
+cd 0-new_project
+dotnet new console
