6aacdd59f507b59c4916ce1f35a9f31eaeb6f24d
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-12 18:58:18 -0500

    modified to required format

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 
-namespace _4_print_integer
+class Program
 {
-    internal class NewBaseType
-    {
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
             int number = 972;
             Console.WriteLine("{0} Mission Street", number);
         }
-    }
 }
