8be636d5f33ce3a71ecec3050a387a1f33a94eab
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-12 17:51:45 -0500

    adding number 4

---
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_integer
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] args)
+        {
+            int number = 972;
+            Console.WriteLine("{0} Mission Street", number);
+        }
+    }
+}
