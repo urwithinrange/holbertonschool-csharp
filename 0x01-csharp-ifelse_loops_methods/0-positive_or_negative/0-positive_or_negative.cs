03aff517710b6c7db0d696b2784544309cd50585
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-13 12:05:33 -0500

    Created DIR and Readme incl. Answer to 1

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        if (number < 0)
+            Console.WriteLine($"{number} is negative");
+        else if (number > 0)
+            Console.WriteLine($"{number} is positive");
+        else
+            Console.WriteLine($"{number} is 0");
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/README.md
@@ -0,0 +1,11 @@
+# 0x01. C# - if/else, loops, functions
+
+## Learning Objectives
+
++ How to use if, else if, else statements
++ How to use while and for loops
++ How to use break and continue statements
++ How to use numeric format strings
++ How to declare methods within a class
++ How to call public methods from another class
++ What is passing by reference and passing by value
