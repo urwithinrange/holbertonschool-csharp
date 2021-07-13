7de26dbd2c87432a70629c9ba655d5e31f6a0fb9
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-13 14:21:37 -0500

    Answered number 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int num = 0; num < 99; num++)
+            Console.Write($"{num} = 0x{num:x0}\n");
+    }
+}
