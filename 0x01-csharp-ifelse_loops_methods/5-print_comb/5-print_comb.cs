4c189984f1e1f0dbb3ac51983635bf36015bbcf2
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-13 14:39:34 -0500

    modified print 99

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -4,9 +4,11 @@ class Program
     static void Main(string[] args)
     {
         for (int num = 00; num <= 99; num++)
-        {    Console.Write($"{num / 10}{num % 10}");
-                if (num < 99)
-                Console.Write(", ");
+        {
+            if (num == 99)
+                Console.Write($"{num} \n");
+            else
+                Console.Write($"{num / 10}{num % 10}, ");
         }
     }
 }
