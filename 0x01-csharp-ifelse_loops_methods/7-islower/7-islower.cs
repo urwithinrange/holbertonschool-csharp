0357fae6e3e4e1011b49f8060949874211aedf1f
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-13 15:30:11 -0500

    Checking answer to checker

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,11 @@
+using System;
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (c >= 'a' && c <+ 'z')
+            return true;
+        else
+            return false;
+    }
+}
