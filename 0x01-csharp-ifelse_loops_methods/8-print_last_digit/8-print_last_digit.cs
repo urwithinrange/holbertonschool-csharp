d5f3642020c613adb6cde19c465f102a99b26919
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-13 15:48:47 -0500

    Answered number 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            number = Math.Abs(number % 10);
+            Console.Write($"{number}");
+            return (number);
+        }
+    }
