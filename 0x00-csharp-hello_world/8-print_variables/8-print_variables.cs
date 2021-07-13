aadeb217ef51aeed64ae1ef258087c63c696bb1f
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-12 20:16:57 -0500

    Answered 8

---
+++ 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+        Console.WriteLine("{0} {2} {1}", int1, str1, str2);
+    }
+}
