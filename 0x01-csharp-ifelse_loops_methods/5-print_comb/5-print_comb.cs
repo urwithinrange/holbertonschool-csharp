7369ec36ae550f0e314d029aa21a9378183009cb
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-13 14:34:49 -0500

    Answered number 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int num = 00; num <= 99; num++)
+        {    Console.Write($"{num / 10}{num % 10}");
+                if (num < 99)
+                Console.Write(", ");
+        }
+    }
+}
