20b7220aefdf1a05dc07fe2c603f01f026e9616f
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-13 14:09:27 -0500

    Answered number 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char alphabet;
+        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+            if (alphabet == 'q' || alphabet == 'e')
+                continue;
+            else
+                Console.Write(alphabet);
+    }
+}
