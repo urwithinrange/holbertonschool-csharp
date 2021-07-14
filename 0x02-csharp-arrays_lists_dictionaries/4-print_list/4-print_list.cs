2bb4ad33ed95130267a62cca2802232c6b3447b9
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-14 17:03:13 -0500

    account for neg size

--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -4,6 +4,12 @@ class List
 {
     public static List<int> CreatePrint(int size)
     {
+        if (size < 0)
+        {   
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+         
         List<int> myList = new List<int>();
         for (int i = 0; i < size; i++)
         {
