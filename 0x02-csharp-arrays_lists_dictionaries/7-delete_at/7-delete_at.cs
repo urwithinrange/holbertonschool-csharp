12eec87f3276223daf1ad525062fcc353a8e740b
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-14 18:16:00 -0500

    Answering 7

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _7_delete_at
 {
-    class Program
+    class List
     {
-        static void Main(string[] args)
+        public static List<int> DeleteAt(List<int> myList, int index)
         {
-            Console.WriteLine("Hello World!");
+            if (myList.Count <= index || index <= 0)
+            {
+                Console.WriteLine("Index is out of range");
+                return myList;
+            }
+            else
+                myList.Remove(myList[index]);
+            return myList;
         }
     }
 }
