4a494258e5bb44d084dd7509641aadf77e8c80c6
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-14 17:29:11 -0500

    6 Answered

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> tflist = new List<bool>();
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] % 2 == 0)
+                tflist.Add(true);
+            else
+                tflist.Add(false);
+        }
+        return tflist;
+    }
+}
