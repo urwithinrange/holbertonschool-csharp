294f1fdfd4fab3054f369a909c163f06fa0c18eb
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-14 14:28:40 -0500

    Created Directory and Answered 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] num = new int[size];
+        for (int i = 0; i < num.Length; i++)
+        {
+            num[i] = i;
+            Console.Write(num [i]);
+            if (i + 1 != num.Length)
+                Console.Write(" ");
+        }
+        Console.Write("\n");
+        return (num);
+    }
+
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/README.md
@@ -0,0 +1,13 @@
+# 0x02. C# - Data Structures: Arrays, Lists, Dictionaries
+
+## Learning Objectives
+
++ How to create and use arrays
++ What are Collections in C#
++ How to create and use lists
++ What is the difference between arrays and lists in C#
++ When would you use an array vs a list
++ How to create and use dictionaries
++ What is the difference between dictionary and hashtables in C#
++ When would you use a dictionary vs a hashtable
++ How to use foreach
