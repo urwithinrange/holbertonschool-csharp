19edbdbaa123e5f69d85cd3f8321eb536e2d031e
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-14 16:58:56 -0500

    Answered 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> myList = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            myList.Add(i);
+            Console.Write(i);
+            if (i + 1 != size)
+                Console.Write(" ");
+        }
+        Console.Write("\n");
+        return myList;
+    }
+
+}
