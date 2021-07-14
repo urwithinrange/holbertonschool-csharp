5c311b8642b83b357fe19337a38c042cd8f97de0
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-14 15:09:27 -0500

    Answered number 1

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        int arlen = array.Length;
+        if (index > arlen || index < 0) {
+            Console.WriteLine("Index out of range");
+            return (-1);
+            }
+        for (int i = 0; i <= index; i++) {
+            if (i == index) {
+                break;
+            }
+        }
+        return (array [index]);
+    }
+}
