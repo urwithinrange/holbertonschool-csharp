030c748d604d0251a0822248c4eae506130cdc0d
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-14 16:29:27 -0500

    Answered 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Array
+    {
+        public static void Reverse(int[] array)
+        {
+            if (array == null || array.Length == 0)
+                {
+                    Console.WriteLine();
+                    return;
+                }
+            int i = array.Length - 1;
+            for (; i >= 0; i--)
+            {
+                Console.Write(array[i]);
+                if (i != 0)
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
