9bb42783e92756b942238050c0d1751fc7801479
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-14 16:10:30 -0500

    Answered 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Array
+    {
+        public static int[] ReplaceElement(int[] array, int index, int n)
+        {
+            if (index < 0 || index >= array.Length) {
+            Console.WriteLine("Index out of range");
+            return array;
+            }
+            else
+            {
+                array[index] = n;
+                return array;
+            }
+        }
+    }
