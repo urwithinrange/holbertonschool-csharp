667d86aed2f501cd3a9b4f022dc96813a9687517
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-15 12:36:09 -0500

    Answered 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+    {
+        public static List<int> DifferentElements(List<int> list1, List<int> list2)
+        {
+//             Write a method that returns a sorted list of all elements present in one or the other list but not both.
+//             Class Name: List
+//             Prototype: public static List<int> DifferentElements(List<int> list1, List<int> list2)
+//             Returns a new sorted list of all elements present in one or the other list but not both of the given lists
+//             You cannot use LINQ
+//             You cannot use SortedList
+            List<int> list3 = new List<int>();
+            for (int i = 0; i < list1.Count; i++)
+                list3.Add(list1[i]);
+            for (int k = 0; k < list2.Count; k++)
+            {
+                for (int j = 0; j < list1.Count; j++)
+                {
+                    if (list1[j] == list2[k])
+                    {
+                        list3.Remove(list1[j]);
+                        break;
+                    }
+                    else if (j + 1 == list1.Count)
+                        list3.Add(list2[k]);
+                }
+            }
+            list3.Sort();
+            return list3;
+        }
+    }
