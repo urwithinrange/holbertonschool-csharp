89af473365a28028d75e79e44bb444b8ceff7028
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-15 12:56:41 -0500

    Answer 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+    {
+        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        {            
+            List<int> newList = new List<int>();
+            for (int i = 0; i < list1.Count; i++)
+            {
+                for (int j = 0; j < list2.Count; j++)
+                {
+                    if (list1[i] == list2[j])
+                    {
+                        newList.Add(list1[i]);
+                    }
+                }
+            }
+            return newList;
+        }
+    }
