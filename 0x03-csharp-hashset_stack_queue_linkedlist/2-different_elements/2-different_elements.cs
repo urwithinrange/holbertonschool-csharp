a113283ed8b2af37c713c29d0ee228966c020bf8
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-15 12:44:22 -0500

    Checker creeping on my comments?

--- 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -4,12 +4,6 @@ class List
     {
         public static List<int> DifferentElements(List<int> list1, List<int> list2)
         {
-//             Write a method that returns a sorted list of all elements present in one or the other list but not both.
-//             Class Name: List
-//             Prototype: public static List<int> DifferentElements(List<int> list1, List<int> list2)
-//             Returns a new sorted list of all elements present in one or the other list but not both of the given lists
-//             You cannot use LINQ
-//             You cannot use SortedList
             List<int> list3 = new List<int>();
             for (int i = 0; i < list1.Count; i++)
                 list3.Add(list1[i]);
