e84eedc505ab2542a140f3b5427bb5024f3e7f7e
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-15 12:00:26 -0500

    Answered 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            int sum = 0;
+            IEnumerable<int> distinctnum = myList.Distinct();
+            foreach (int num in distinctnum)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
