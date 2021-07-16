ac4a5d36c564c6db705dac1c7847dc4b54442a3a
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-15 20:01:49 -0500

    Answered 4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int size = aQueue.Count;
+        Console.WriteLine($"Number of items: {size}");
+        if (size == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine($"First item: {aQueue.Peek()}");
+        aQueue.Enqueue(newItem);
+        if (aQueue.Contains(search))
+        {
+            Console.WriteLine($"Queue contains \"{search}\": True");
+            while (size != 0 && (aQueue.Peek() != search || aQueue.Peek() == search) && aQueue.Contains(search))
+                aQueue.Dequeue();
+        }    
+        else
+            Console.WriteLine($"Queue contains \"{search}\": False");
+        return aQueue;   
+    }
+}
