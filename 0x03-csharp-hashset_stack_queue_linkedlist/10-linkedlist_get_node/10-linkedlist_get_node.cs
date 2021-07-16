616737adc611bc9fdd4f80e84d0f49df6640e004
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-15 21:53:09 -0500

    Answered 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int pos = 0;
+        foreach(int item in myLList)
+        {
+            if (pos == n)
+                return item;
+            pos++;
+        }
+        return 0;
+    }
+}
