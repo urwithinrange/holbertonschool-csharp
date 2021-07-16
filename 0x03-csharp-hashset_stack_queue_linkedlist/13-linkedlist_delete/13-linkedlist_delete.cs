25d2b78e91b74a5b50ecf1c804a7da0a33d63f45
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-15 22:54:18 -0500

    Answered 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> ll = myLList.First;
+        for (int i = 0; i < myLList.Count; i++)
+        {
+            if (i == index)
+            {
+                myLList.Remove(ll.Value);
+                return;
+            }
+            ll = ll.Next;
+        }        
+    }
+}
