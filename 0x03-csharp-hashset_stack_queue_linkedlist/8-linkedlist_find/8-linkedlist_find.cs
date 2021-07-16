96a9087beedbeb931d21fc2e48496631e2063926
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-15 21:32:31 -0500

    Answered 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int pos = 0;
+        foreach (int item in myLList)
+        {
+            if (item == value)
+                return pos;
+            pos++;
+
+        }
+        return -1;
+    }
+}
