859c85e1f0e643f63d2ab25534d51800b2d21505
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-15 21:57:36 -0500

    Answered 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+        foreach(int num in myLList)
+            sum += num;
+        return sum;
+    }
+}
