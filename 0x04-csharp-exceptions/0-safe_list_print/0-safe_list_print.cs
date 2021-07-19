699c4b23db79bcf4e441aecca25ed356f2592c4c
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-19 13:14:42 -0500

    Answered 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+        while (count <= n)
+        {
+             try
+             {
+                 if (count == n || myList.Count == count)
+                 {
+                      throw new Exception();
+                 }
+                 Console.WriteLine($"{myList[count]}");
+                 count++;
+             }
+             catch
+             {
+                 return count;
+             }
+        }
+        return count;
+    }
+}
