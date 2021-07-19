3321da3f88ae537bb7f024511b38cabf48d16cf5
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-19 13:25:29 -0500

    Answered 1

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int div = 0;
+        try
+        {
+            if (b == 0)
+                throw new DivideByZeroException();
+            div = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {div}");
+        }
+    }
+}
