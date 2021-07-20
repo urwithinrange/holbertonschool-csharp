c1068cd5e2b6b548a155fa7d3c75193bd8c9fefe
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-20 14:47:57 -0500

    Cash wrap

--- 0x05-csharp-structs_enums/3-dog/3-dog.cs
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -19,6 +19,6 @@ public struct Dog
     }
     public override string ToString()
     {
-        return base.ToString() + " Name: " + name.ToString() + "\n" + "Age: " + age.ToString() + "\n" + "Owner: " + owner.ToString() + "\n" + "Rating: " + rating.ToString();
+        return $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
     }
 }
