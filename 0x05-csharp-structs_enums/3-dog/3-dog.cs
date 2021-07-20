24a0d8d4fafc89f3e76342ff46bf670b2597b9ce
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-20 14:33:22 -0500

    Answered 3

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,24 @@
+﻿public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+    public override string ToString()
+    {
+        return base.ToString() + " Name: " + name.ToString() + "\n" + "Age: " + age.ToString() + "\n" + "owner: " + owner.ToString() + "\n" + "Rating: " + rating.ToString();
+    }
+}
