c076e03f52214df3119f494e9a91bb6e06602491
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-21 11:07:16 -0500

    Answered 5

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        private int health;
+        private string name = "(No name)";        
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (value != null)
+                    name = value;
+            }
+        }
+        
+        public Zombie()
+        {
+        }
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArithmeticException("Health must be greater than or equal to 0");
+            this.health = value;
+        }
+        public int GetHealth()
+        {
+            return health;
+        }
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+    }
+}
