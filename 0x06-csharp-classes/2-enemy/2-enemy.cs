22736df0ceef910bd3aca091ac043a82296fc22e
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-21 10:40:45 -0500

    Answered 2

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+        }
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArithmeticException("Health must be greater than or equal to 0");
+            this.health = value;
+        }
+    }
+}
