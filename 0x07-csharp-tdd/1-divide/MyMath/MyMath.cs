d5e39cb4fc87e86b47da0ea7ba00488aaad54895
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-25 19:04:41 -0500

    Update MyMath.cs

--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -3,13 +3,13 @@ using System;
 
 namespace MyMath
 {
-    ///<summary>Matrix Class.</summary>
+    /// <summary>Matrix Class.</summary>
     public class Matrix
     {
-        ///<summary>Gets quotient of a matrix's values divided by num.</summary>
-        ///<param name="matrix">Array of a fixed size.</param>
-        ///<param name="num">Is the divided const.</param>
-        ///<returns>a new matrix containing divided elements.</returns>        
+        /// <summary>Gets quotient of a matrix's values divided by num.</summary>
+        /// <param name="matrix">Array of a fixed size.</param>
+        /// <param name="num">Is the divided const.</param>
+        /// <Returns>a new matrix containing divided elements.</Returns>        
         public static int[,] Divide(int[,] matrix, int num)
         {
             if (matrix == null || num == 0)
