623c4afd93c46361c30a0e950f40267cec7d15da
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-25 19:33:20 -0500

    removed extra line

--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -1,12 +1,11 @@
-﻿
-using System;
+﻿using System;
 
 namespace MyMath
 {
     /// <summary>Matrix Class.</summary>
     public class Matrix
     {
-        /// <summary>Gets quotient of<paramref name="matrix"/> values divided by <paramref name="num"/>.</summary>
+        /// <summary>Gets quotient of <paramref name="matrix"/> values divided by <paramref name="num"/>.</summary>
         /// <param name="matrix">Array of a fixed size.</param>
         /// <param name="num">Is the divided const.</param>
         /// <returns>matrix containing divided elements.</returns>
