91972de294c517790355980e4fc7ec949098ffdc
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-27 14:11:32 -0500

    Answered 4

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Animal is Baseclass</summary>
+class Animal
+{
+    //
+}
+/// <summary>Animal is Baseclass</summary>
+class Dog : Animal
+{
+    //
+}
