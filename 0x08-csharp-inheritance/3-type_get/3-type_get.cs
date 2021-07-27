4d80ee34d1b3561b615d268c24871e2f458002e6
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-27 14:06:32 -0500

    Answered 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+/// <summary>Obj class</summary>
+class Obj
+{
+    /// <summary>Method that prints the names of the available properties of an object.</summary>
+    /// <param name="myObj">The being reffered to</param>
+    public static void Print(object myObj)
+    {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        Console.WriteLine($"{t.Name} Properties:");
+        foreach (PropertyInfo i in t.GetProperties())
+        {
+            Console.WriteLine(i.Name);
+        }
+        Console.WriteLine($"{t.Name} Methods:");
+        foreach (MethodInfo x in t.GetMethods())
+        {
+            Console.WriteLine(x.Name);
+        }
+    }
+}
