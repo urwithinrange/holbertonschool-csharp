74ed67d591300095085faa5ad1f8e68ad05c370b
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-27 13:41:00 -0500

    Answered 2

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Obj class</summary>
+class Obj
+{
+    /// <summary>Method that returns true or false.</summary>
+    /// <param name="derivedType">The subclass being checked.</param>
+    /// <param name="baseType">The baseclass being ccompared to.</param>
+    /// <returns>True or false bool.</returns>
+
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+
+        if (derivedType.IsSubclassOf(baseType))
+            return true;
+        return false;
+    }
+}
