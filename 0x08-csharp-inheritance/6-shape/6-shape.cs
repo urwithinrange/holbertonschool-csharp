08c5eaeca390f6a26527732f881976aadec5f3a3
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-07-27 15:04:25 -0500

    Answered 6

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Shapeclass</summary>
+class Shape
+{
+    /// <summary>Method returns new Exception</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    public int Width
+    {
+        get { return width; }
+        set {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+            
+        }
+    }
+    public int Height
+    {
+        get { return height; }
+        set {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+            
+        }
+    }
+}
