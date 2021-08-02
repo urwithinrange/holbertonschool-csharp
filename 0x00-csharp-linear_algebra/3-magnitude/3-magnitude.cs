b6aa0af7875395eed5a0eac396c4dbd4c35cd165
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-08-02 13:19:55 -0500

    initial push

---
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        double result;
+        if (vector.Length == 2) {
+            return (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
+            }
+        else if (vector.Length == 3) {
+            return (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
+            }
+        else
+            return -1;
+    }
+}
