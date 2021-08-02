fe9600229874e01d5806792ecc78d0defdc0a657
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-08-02 13:29:19 -0500

    forgot result

--- 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -5,10 +5,10 @@ class VectorMath
     {
         double result;
         if (vector.Length == 2) {
-            return (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
+            return result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
             }
         else if (vector.Length == 3) {
-            return (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
+            return result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
             }
         else
             return -1;
