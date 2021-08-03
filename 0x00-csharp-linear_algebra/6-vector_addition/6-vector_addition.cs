292f2fce91bd8c2fde553d58e7b51777ef810509
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-08-02 22:56:15 -0500

    Answered 6

---
+++ 0x00-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length == 2 && vector2.Length == 2)
+        {
+            return new double[] {vector1[0] + vector2[0], vector1[1] = vector2[1]};
+        }
+        else if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            return new double[] {vector1[0] + vector2[0], vector1[1] = vector2[1], vector1[2] + vector2[2]};
+        }
+        return -1;
+    }
+}
