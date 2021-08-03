4d40452012587bef46e4c3dba9be3aa19da5cc37
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-08-02 23:40:23 -0500

    update for uneven matricies

--- 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -6,6 +6,10 @@ class MatrixMath
     /// <summary>add two matrices</summary>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+        {
+            return new double[,] {{-1}};
+        }
         if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2)
         {
             return new double[,] {{matrix1[0, 0] + matrix2[0, 0], matrix1[0, 1] + matrix2[0, 1]}, {matrix1[1, 0] + matrix2[1, 0], matrix1[1, 1] + matrix2[1, 1]}};
