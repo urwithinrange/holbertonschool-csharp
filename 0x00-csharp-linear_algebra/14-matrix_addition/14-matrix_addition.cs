aa5fd2df819ea734361b5e1ff852d0cf72b3e233
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-08-02 23:36:26 -0500

    Answered 14

---
+++ 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>Matrix math</summary>
+class MatrixMath
+{
+    /// <summary>add two matrices</summary>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2)
+        {
+            return new double[,] {{matrix1[0, 0] + matrix2[0, 0], matrix1[0, 1] + matrix2[0, 1]}, {matrix1[1, 0] + matrix2[1, 0], matrix1[1, 1] + matrix2[1, 1]}};
+        }
+        if (matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3)
+        {
+            return new double[,] {{matrix1[0, 0] + matrix2[0, 0], matrix1[0, 1] + matrix2[0, 1], matrix1[0, 2] + matrix2[0, 2]}, {matrix1[1, 0] + matrix2[1, 0], matrix1[1, 1] + matrix2[1, 1], matrix1[1, 2] + matrix2[1, 2]}, {matrix1[2, 0] + matrix2[2, 0], matrix1[2, 1] + matrix2[2, 1], matrix1[2, 2] + matrix2[2, 2]}};
+        }
+        return new double[,] {{-1}};
+    }
+}
