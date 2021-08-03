72f6042e5b7198bfde3bc4ff8e478b6f5ce27b20
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-08-02 21:12:02 -0500

    Changing the syntax of the equations

--- 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -5,10 +5,12 @@ class VectorMath
     {
         double result;
         if (vector.Length == 2) {
-            return result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
+            result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)));
+            return Math.Round(result, 2);
             }
         else if (vector.Length == 3) {
-            return result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
+            result = (Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)));
+            return Math.Round(result, 2);
             }
         else
             return -1;
