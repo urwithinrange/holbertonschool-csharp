b12c71445c599b95de1c1dfd2c06999974b7d809
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-08-02 23:04:03 -0500

    fixing the error of new b

--- 0x00-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x00-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -5,11 +5,11 @@ class VectorMath
     {
         if (vector1.Length == 2 && vector2.Length == 2)
         {
-            return new double[] {vector1[0] + vector2[0], vector1[1] = vector2[1]};
+            return new double[] {vector1[0] + vector2[0], vector1[1] + vector2[1]};
         }
         else if (vector1.Length == 3 && vector2.Length == 3)
         {
-            return new double[] {vector1[0] + vector2[0], vector1[1] = vector2[1], vector1[2] + vector2[2]};
+            return new double[] {vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2]};
         }
         return new double[] {-1};
     }
