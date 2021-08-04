8734ab193cfdde4c6d4b3988000f11819c482baa
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-08-04 16:39:41 -0500

    Initial push on 0x02

---
+++ 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+/// <summary>Abstract class for Base</summary>
+public abstract class Base
+{
+    /// <summary>public property</summary>
+    public string name;
+    
+    public override string ToString()
+    {
+        return name + " is a " + GetType();
+    }        
+}
---
+++ 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,54 @@
+﻿using System;
+/// <summary>Abstract class for Base</summary>
+public abstract class Base
+{
+    /// <summary>Public property declaration</summary>
+    public string name;
+    /// <summary>Method to override ToString</summary>
+    public override string ToString()
+    {
+        return name + " is a " + GetType();
+    }
+}
+/// <summary>Interface</summary>
+interface IInteractive
+{
+    /// <summary>Interface method</summary>
+    void Interact();
+}
+/// <summary>Interface</summary>
+interface IBreakable
+{
+    /// <summary>Public property declaration</summary>
+    int durability { get; set; }
+    /// <summary>Interface method</summary>
+    void Break();
+}
+/// <summary>Interface</summary>
+interface ICollectable
+{
+    /// <summary>Public property declaration</summary>
+    bool isCollected { get; set; }
+    /// <summary>Interface method</summary>
+    void Collect();
+}
+/// <summary>public class that interacts from base class</summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    /// <summary>Read-write instance property.</summary>
+    public int durability { get; set; }
+    /// <summary>Read-write instance property.</summary>
+    public bool isCollected { get; set; }
+    /// <summary>Inherited Method</summary>
+    public void Interact()
+    {
+    }
+    /// <summary>Inherited Method</summary>
+    public void Collect()
+    {
+    }
+    /// <summary>Inherited Method</summary>
+    public void Break()
+    {
+    }
+}
