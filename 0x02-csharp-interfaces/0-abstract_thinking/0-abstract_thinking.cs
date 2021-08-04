43d97be1835052d7fed06116c6e4c192d4fc985b
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-08-04 16:55:16 -0500

    XML for public

--- 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -2,11 +2,11 @@
 /// <summary>Abstract class for Base</summary>
 public abstract class Base
 {
-    /// <summary>public property</summary>
-    public string name;
-    
+    /// <summary>Public property declaration</summary>
+    public string name { get; set; }
+    /// <summary>Method to override ToString</summary>
     public override string ToString()
     {
         return name + " is a " + GetType();
-    }        
+    }
 }
