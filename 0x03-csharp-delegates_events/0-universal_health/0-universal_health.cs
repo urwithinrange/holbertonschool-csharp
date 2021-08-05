ba965dcaaf7cbbeb2bb4f93542127a8141353d34
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-08-05 16:13:45 -0500

    removed this

--- 0x03-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x03-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -15,7 +15,7 @@ public class Player
         if (maxHp <= 0f)
         {
            System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
-           this.maxHp = 100f;
+           maxHp = 100f;
         }
         this.name = name;
         this.maxHp = maxHp;
