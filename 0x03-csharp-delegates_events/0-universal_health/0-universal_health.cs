76481cb828b114152135b1e6cb9e47588bd0239d
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-08-05 16:07:21 -0500

    First push

---
+++ 0x03-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,29 @@
+﻿using System;
+/// <summary>class Player</summary>
+public class Player
+{
+    /// <summary>class Player</summary>
+    private string name { get; set; }
+    /// <summary>class Player</summary>
+    private float maxHp { get; set; }
+    /// <summary>class Player</summary>
+    private float hp { get; set; }
+
+    /// <summary>Player constructor</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0f)
+        {
+           System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+           this.maxHp = 100f;
+        }
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+    /// <summary>Method for printing health</summary>
+    public void PrintHealth()
+    {
+        System.Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
---
+++ 0x03-csharp-delegates_events/README.md
@@ -0,0 +1,11 @@
+# 0x03. C# - Delegates, Events
+
+## Learning Objectives
+
++ What are delegates and how to use them
++ What are anonymous methods and how to use them
++ What is multicasting
++ What is the difference between delegates and interfaces
++ What is a callback
++ What are events and how to use them
++ What is asynchronous programming
